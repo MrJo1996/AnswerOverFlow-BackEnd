8386c10d744c2363edab8f2e164dc3e39bf561bf
dani2097 <danilo.sprovieri9@gmail.com>
2020-04-04 20:39:34 +0200

    costruct

--- DB/DBUtenti.php
+++ DB/DBUtenti.php
@@ -1,7 +1,12 @@
 <?php
 
 class DBUtenti
-{
-   
+{ 	private $connection;
+       public function __construct()
+    {
+        //Setup della connessione col DB
+        $db = new DBConnectionManager();
+        $this->connection = $db->runConnection();
+    }
 
 ?>
