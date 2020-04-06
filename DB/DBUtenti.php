eabd65b74a28489a9605ee1b177235ede8613f11
Chris <c.peluso1@studenti.unimol.it>
2020-04-06 12:08:41 +0200

    Creazione delle variabili contenente le tabelle e i relativi campi indicizzati con appunto i nomi delle tabelle

--- DB/DBUtenti.php
+++ DB/DBUtenti.php
@@ -1,12 +1,96 @@
 <?php
 
 class DBUtenti
-{ 	private $connection;
-       public function __construct()
+{
+    private $connection;
+    private $tabelleDB = [ //Array delle tabelle
+        "utente",
+        "stats",
+        "categoria",
+        "sottocategoria",
+        "domanda",
+        "risposta",
+        "sondaggio",
+        "scelta",
+        "chat",
+        "messaggio"
+    ];
+    private $campiTabelleDB = [ //Campi delle tabelle (array bidimensionale indicizzato con key)
+        "utente" => [
+            "email",
+            "username",
+            "password",
+            "nome",
+            "cognome",
+            "bio",
+            "attivo"
+        ],
+        "stats" => [
+            "cod_utente",
+            "cod_categoria",
+            "mediavoto"
+        ],
+        "categoria" => [
+            "codice_categoria",
+            "titolo"
+        ],
+        "sottocategoria" => [
+            "codice_sottocategoria",
+            "cod_categoria",
+            "titolo"
+        ],
+        "domanda" => [
+            "codice_domanda",
+            "dataeora",
+            "timer",
+            "titolo",
+            "descrizione",
+            "cod_utente",
+            "cod_categoria"
+        ],
+        "risposta" => [
+            "codice_risposta",
+            "descrizione",
+            "valutazione",
+            "cod_utente",
+            "cod_domanda"
+        ],
+        "sondaggio" => [
+            "codice_sondaggio",
+            "dataeora",
+            "titolo",
+            "cod_utente",
+            "cod_categoria"
+        ],
+        "scelta" => [
+            "codice_scelta",
+            "descrizione",
+            "cod_sondaggio"
+        ],
+        "chat" => [
+            "codice_chat",
+            "cod_utente0",
+            "cod_utente1"
+        ],
+        "messaggio" => [
+            "codice_messaggio",
+            "dataeora",
+            "testo",
+            "visualizzato",
+            "cod_chat"
+        ]
+    ];
+    //Costruttore
+    public function __construct()
     {
         //Setup della connessione col DB
         $db = new DBConnectionManager();
         $this->connection = $db->runConnection();
     }
 
+    //---- METODI PER GESTIRE LE QUERY ----
+
+
+
+}
 ?>
