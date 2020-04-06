8dda3efa3f4be172c39c78fa22c9339871fbad60
Chris <c.peluso1@studenti.unimol.it>
2020-04-06 14:45:26 +0200

    Creazioni delle sessioni per svolgere le query a me assegnate, aggiornaStats da finire

--- DB/DBUtenti.php
+++ DB/DBUtenti.php
@@ -28,7 +28,7 @@ class DBUtenti
         "stats" => [
             "cod_utente",
             "cod_categoria",
-            "mediavoto"
+            "media_voto"
         ],
         "categoria" => [
             "codice_categoria",
@@ -90,7 +90,122 @@ class DBUtenti
 
     //---- METODI PER GESTIRE LE QUERY ----
 
+    //Funzione di controllo presenza email per recuperare la password
+    public function recuperoPsw($email)
+    {
+        $utenteTab = $this->tabelleDB[0]; //Tabella per la query
+        $campi = $this->campiTabelleDB[$utenteTab]; //Campi per la query
+        //QUERY: "SELECT email FROM utente WHERE email = ?
+        $query = (
+            "SELECT " .
+            $campi[0] . " " .
+            "FROM " .
+            $utenteTab . " " .
+            "WHERE " .
+            $campi[0] . " = ? "
+        );
+        //Invio la query
+        $stmt = $this->connection->prepare($query);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        //Ricevo la risposta del DB
+        $stmt->store_result();
+        //Se ha trovato un match tra la mail inserita e il DB restituisce una bool TRUE
+        return $stmt->num_rows > 0;
+    }
 
+    //Una volta che l'utente mi ha confermato la mail inviata cambio la password
+    public function recuperaPassword($email, $password)
+    {
+        $password = hash('sha256', $password);
+
+        $utenteTab = $this->tabelleDB[0];
+        $campi = $this->campiTabelleDB[$utenteTab];
+        //QUERY:  "UPDATE TABLE SET password = ? WHERE email = ?"
+        $query = (
+            "UPDATE " .
+            $utenteTab . " " .
+            "SET " .
+            $campi[2] . " = ? " .
+            "WHERE " .
+            $campi[0] . " = ?"
+        );
+        //Invio la query
+        $stmt = $this->connection->prepare($query);
+        $stmt->bind_param("ss", $password, $email);
+        //La funzione termina con l'esecuzione della query
+        return $stmt->execute();
+    }
+
+    //Seleziono tutto il contenuto di risposta secondo un determinato ID
+    public function visualizzaRisposta($id_risposta)
+    {
+        $rispostaTab = $this->tabelleDB[5];
+        $campi = $this->campiTabelleDB[$rispostaTab];
+        //QUERY: "SELECT * FROM `risposta` WHERE ID = 'value'"
+        $query = (
+            "SELECT " .
+            "* " .
+            "FROM " .
+            $rispostaTab . " " .
+            "WHERE " .
+            $campi[0] . " = ?"
+        );
+        //Invio la query
+        $stmt = $this->connection->prepare($query);
+        $stmt->bind_param("i", $id_risposta);
+        //La funzione termina con l'esecuzione della query
+        return $stmt->execute();
+    }
+
+    //Prendo la domanda alla quale una risposta fa riferimento
+    public function aChiAppartieniRisposta($id_risposta)
+    {
+        $rispostaTab = $this->tabelleDB[5];
+        $campi = $this->campiTabelleDB[$rispostaTab];
+        //QUERY: SELECT cod_domanda FROM `risposta` WHERE ID = 'value'
+        $query = (
+            "SELECT " .
+            $campi[4] . " " .
+            "FROM " .
+            $rispostaTab . " " .
+            "WHERE " .
+            $campi[0] . " = ?"
+        );
+        //Invio la query
+        $stmt = $this->connection->prepare($query);
+        $stmt->bind_param("i", $id_risposta);
+        //La funzione termina con l'esecuzione della query che mi restituisce il codice della domanda
+        return $stmt;
+    }
+
+    //Prendo la categoria della domanda
+    public function categoriaDomanda($id_domanda)
+    {
+        $domandaTab = $this->tabelleDB[4];
+        $campi = $this->campiTabelleDB[$domandaTab];
+        //QUERY: SELECT 'cod_categoria' FROM `domanda` WHERE codice = 'value'
+        $query = (
+            "SELECT " .
+            $campi[6] . " " .
+            "FROM " .
+            $domandaTab . " " .
+            "WHERE " .
+            $campi[0] . " = ?"
+        );
+        //Invio la query
+        $stmt = $this->connection->prepare($query);
+        $stmt->bind_param("i", $id_domanda);
+        //La funzione termina con l'esecuzione della query che mi restituisce il codice della categoria
+        return $stmt;
+    }
+    //Aggiorno le valutazioni in base all'esito ricevuto dalla risposta data
+    public function aggiornaStats($id_utente, $id_categoria, $valutazione)
+    {
+        $statsTab = $this->tabelleDB[1];
+        $campi = $this->campiTabelleDB[$statsTab];
+        //QUERY: SELECT 'mediavoto' AND numero_risposte WHERE cod_utente = ? AND cod_categoria = ?
+    }
 
 }
 ?>
