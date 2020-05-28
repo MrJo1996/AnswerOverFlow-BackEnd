06423599ccd3c7ea56470b766e2412bacbfaa0c6
frankiafi <francesco.iafi@gmail.com>
2020-05-28 19:07:13 +0200

    piccole correzioni

--- Helper/EmailHelper/EmailHelperAltervista.php
+++ Helper/EmailHelper/EmailHelperAltervista.php
@@ -15,7 +15,7 @@ class EmailHelperAltervista
         $emailTo = $email;
         $subject = "AnswerOverFLow - Recupero Password";
         $message   = '<html><body><h1>AnswerOverFLow</h1><div>';
-        $message   .= $messaggio.':<br/><br/><b>'.$password.'</div><br/><div>Avvia la tua app /*'.$linkLogin.'*/ ed accedi con la nuova password.</div></body></html>';
+        $message   .= $messaggio.':<br/><br/><b>'.$password.'</body></html>';
         $headers = "MIME-Version: 1.0\r\n";
         $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
